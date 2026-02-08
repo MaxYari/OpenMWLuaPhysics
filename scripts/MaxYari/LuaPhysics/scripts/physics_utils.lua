3db913dd1c43cc9df3d7f52a1ac8ed18d29e6ee8
Maksim Eremenko <maksyarik@gmail.com>
2026-02-08 12:20:29 +0100

    3rd person pickup distance fix

--- scripts/MaxYari/LuaPhysics/scripts/physics_utils.lua
+++ scripts/MaxYari/LuaPhysics/scripts/physics_utils.lua
@@ -84,11 +84,11 @@ end
 module.slerpRotation = slerpRotation
 
 
-
+local centerVector = util.vector2(0.5, 0.5)
 local function GrabObject()
     local position = camera.getPosition()
-    local direction = camera.viewportToWorldVector(util.vector2(0.5, 0.5)):normalize()
-    local pickupDistance = grabDistance
+    local direction = camera.viewportToWorldVector(centerVector):normalize()
+    local pickupDistance = amera.getThirdPersonDistance() + grabDistance
     local castResult = nearby.castRenderingRay(position, position + direction * pickupDistance)
     local object = castResult.hitObject
  
